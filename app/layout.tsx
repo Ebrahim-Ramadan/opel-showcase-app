621a2a75466a7b4e3519544a292e1b613aa0cdf7
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2025-11-07 17:01:20 +0200

    ass

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={`font-sans antialiased`}>
-        <Toaster position="bottom-right" />
+        <Toaster position="bottom-right" richColors  />
         {children}
         <Analytics />
       </body>
--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -68,12 +68,12 @@ export default function Dashboard({ currentPage, setCurrentPage, onLogout }: Das
             {/* Cart Button */}
             <button
               onClick={() => setNavigateToCheckout(true)}
-              className="group relative px-4 py-2 rounded-lg bg-purple-600/30 text-purple-300 border border-purple-500/50 hover:bg-purple-600/50 transition-all flex items-center gap-2"
+              className=" relative px-4 py-2 rounded-lg bg-purple-600/30 text-purple-300 border border-purple-500/50 hover:bg-purple-600/50 transition-all flex items-center gap-2"
             >
-              <ShoppingCart className="w-4 h-4" />
-              <ArrowRight className="w-3 h-3 group/hover:scale-105" />
+              <ShoppingCart className="w-5 h-5" />
+              {/* <ArrowRight className="w-3 h-3 group/hover:scale-105" /> */}
               {isLoaded && cart.length > 0 && (
-                <span className="absolute top-0 right-0 bg-pink-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+                <span className="absolute -top-1 -right-1 bg-pink-500 text-white text-xs font-bold rounded-full w-4 h-4 flex items-center justify-center">
                   {cart.length}
                 </span>
               )}
